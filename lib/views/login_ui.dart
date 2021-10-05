a88375c2ee775513971eb90a8189f1f5079664a1
s6135410017 <s6135410017@sau.ac.th>
2021-10-05 21:00:59 +0700

    CODE V2

--- lib/views/login_ui.dart
+++ lib/views/login_ui.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class LoginUI extends StatefulWidget {
   const LoginUI({Key? key}) : super(key: key);
@@ -281,6 +282,149 @@ class _LoginUIState extends State<LoginUI> {
                     ),
                   ],
                 ),
+              ),
+              SizedBox(
+                height: 40.0,
+              ),
+              ElevatedButton.icon(
+                onPressed: () {},
+                icon: Icon(
+                  FontAwesomeIcons.facebookF,
+                ),
+                label: Text(
+                  'Facebook',
+                  style: TextStyle(
+                    fontFamily: 'Kanit',
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFF1877f2),
+                  fixedSize: Size(
+                    200.0,
+                    50.0,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 40.0,
+              ),
+              ElevatedButton.icon(
+                onPressed: () {},
+                icon: Icon(
+                  FontAwesomeIcons.googlePlusG,
+                ),
+                label: Text(
+                  'Google',
+                  style: TextStyle(
+                    fontFamily: 'Kanit',
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFFdb4a39),
+                  fixedSize: Size(
+                    200.0,
+                    50.0,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 40.0,
+              ),
+              ElevatedButton.icon(
+                onPressed: () {},
+                icon: Icon(
+                  FontAwesomeIcons.apple,
+                ),
+                label: Text(
+                  'Apple ID',
+                  style: TextStyle(
+                    fontFamily: 'Kanit',
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFF333333),
+                  fixedSize: Size(
+                    200.0,
+                    50.0,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  OutlinedButton(
+                    onPressed: () {},
+                    child: Icon(
+                      FontAwesomeIcons.instagram,
+                    ),
+                    style: OutlinedButton.styleFrom(
+                      side: BorderSide(
+                        color: Color(0xFF8a3ab9),
+                      ),
+                      fixedSize: Size(
+                        65.0,
+                        65.0,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                          80.0,
+                        ),
+                      ),
+                      primary: Color(0xFF8a3ab9),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 15.0,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Icon(
+                      FontAwesomeIcons.twitter,
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: Size(
+                        80.0,
+                        80.0,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                          80.0,
+                        ),
+                      ),
+                      primary: Color(0xFF00aced),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 15.0,
+                  ),
+                  OutlinedButton(
+                    onPressed: () {},
+                    child: Icon(
+                      FontAwesomeIcons.linkedin,
+                    ),
+                    style: OutlinedButton.styleFrom(
+                      side: BorderSide(
+                        color: Color(0xFF077b5),
+                      ),
+                      fixedSize: Size(
+                        65.0,
+                        65.0,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                          80.0,
+                        ),
+                      ),
+                      primary: Color(0xFF0077b5),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10.0,
               ),
             ],
           ),
